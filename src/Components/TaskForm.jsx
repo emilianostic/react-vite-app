08d6af7cc5666f1673b787c8bb6e5d385d11e2b8
emilianostic <emilianostic@gmail.com>
2022-12-11 23:11:35 -0300

    cambios del 11 de diciembre

---
+++ src/Components/TaskForm.jsx
@@ -0,0 +1,37 @@
+import { useState, useContext } from "react";
+import {TaskContext} from '../context/TaskContext'
+function TaskForm() {
+  const [title, setTitle] = useState("");
+  /*  Esto de arriba es const title = "" */
+  const [description, setDescription] = useState("");
+
+  const {createTask}= useContext (TaskContext)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createTask({
+      title,
+      description,
+    });
+    setTitle("");
+    setDescription("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Escribe tu tarea"
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        autoFocus
+      />
+      <textarea
+        placeholder="Escribe la descripción de la tarea"
+        onChange={(e) => setDescription(e.target.value)}
+        value={description}
+      ></textarea>
+      <button>Guardar</button>
+    </form>
+  );
+}
+
+export default TaskForm;
